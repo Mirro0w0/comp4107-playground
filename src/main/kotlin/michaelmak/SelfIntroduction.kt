b87737b6933e773780b7500528d3d3021cc1b6a3
Mirro0w0 <21234663@life.hkbu.edu.hk>
2026-02-25 16:59:47 +0800

    Updated Name

--- src/main/kotlin/michaelmak/SelfIntroduction.kt
+++ src/main/kotlin/michaelmak/SelfIntroduction.kt
@@ -2,7 +2,7 @@ package hk.edu.hkbu.comp.comp4107.playground.michaelmak
 
 class SelfIntroduction {
     fun selfIntroduction() {
-        println("Hello, my name is <Your Full Name>.")
+        println("Hello, my name is Michael Mak.")
         println("I am a student at HKBU.")
         println("I am studying Computer Science.")
         println("I am interested in machine learning.")
